652ec9aa8c4ff1044a73d7c33109e0d07d3865fa
Eric Lambrecht <eric@feed.fm>
2017-05-21 15:13:03 -0700

    unused

--- iOS-RadioPlayer-2/Player/FMStationCollectionViewController.m
+++ iOS-RadioPlayer-2/Player/FMStationCollectionViewController.m
@@ -272,19 +272,5 @@ minimumLineSpacingForSectionAtIndex:(NSInteger)section {
     return YES;
 }
 
-/*
-// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
-	return NO;
-}
-
-- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	return NO;
-}
-
-- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
-	
-}
-*/
 
 @end
